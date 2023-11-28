2d9a4c85122d628ecf4cc845ea7a4094d9ee1c57
Georgi-K-Sotirov <Goshko45713>
2023-11-28 22:50:37 +0200

    For Loop - Exercise

--- While Loop - Exercise/coins.py
+++ While Loop - Exercise/coins.py
@@ -1,3 +1,32 @@
 inserted_money = float(input())
-coins_couner = 0
+inserted_money = int(inserted_money * 100)
+coins_counter = 0
 
+while inserted_money > 0:
+    if inserted_money >= 200:
+        coins_counter += 1
+        inserted_money -= 200
+    elif inserted_money >= 100:
+        coins_counter += 1
+        inserted_money -= 100
+    elif inserted_money >= 50:
+        coins_counter += 1
+        inserted_money -= 50
+    elif inserted_money >= 20:
+        coins_counter += 1
+        inserted_money -= 20
+    elif inserted_money >= 10:
+        coins_counter += 1
+        inserted_money -= 10
+    elif inserted_money >= 5:
+        coins_counter += 1
+        inserted_money -= 5
+    elif inserted_money >= 2:
+        coins_counter += 1
+        inserted_money -= 2
+    elif inserted_money >= 1:
+        coins_counter += 1
+        inserted_money -= 1
+        break
+
+print(f"{coins_counter:.0f}")
